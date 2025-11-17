f1f616d9720bf070b793d4df1f78b270809813da
Corran Webster <cwebster@unital.dev>
2025-11-17 15:35:54 +0000

    extmod/modframebuf.c: Fix code style

--- tests/extmod/framebuf16.py
+++ tests/extmod/framebuf16.py
@@ -4,9 +4,7 @@ except ImportError:
     print("SKIP")
     raise SystemExit
 
-if (
-    sys.byteorder == "little" and framebuf.RGB565 == framebuf.RGB565_LE
-) or (
+if (sys.byteorder == "little" and framebuf.RGB565 == framebuf.RGB565_LE) or (
     sys.byteorder == "big" and framebuf.RGB565 == framebuf.RGB565_BE
 ):
     print("Native format matches expected value.")
